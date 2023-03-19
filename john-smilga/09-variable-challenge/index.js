f8686fd72434c0658c501ee95f993db9cfc6fa51
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-19 16:09:28 +0530

    add

---
+++ john-smilga/09-variable-challenge/index.js
@@ -0,0 +1,25 @@
+let firstName, lastName;
+let address = "main Street";
+
+address = "first street";
+
+console.log(firstName, lastName, address);
+
+let street, country;
+
+street = "Pratap Nagar";
+country = " India";
+
+let fullMailingAddress = street + country;
+console.log(fullMailingAddress);
+
+let score1, score2, score3;
+score1 = 56;
+score2 = 66;
+score3 = 76;
+let total = score1 + score2 + score3;
+
+console.log(total);
+
+let avg = total / 3;
+console.log(avg);
