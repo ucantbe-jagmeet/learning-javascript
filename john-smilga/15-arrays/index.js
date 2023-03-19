5f0d331c6c4901bc52d933bb5c8a5518d82b5243
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-19 16:11:04 +0530

    add

---
+++ john-smilga/15-arrays/index.js
@@ -0,0 +1,12 @@
+// Arrays
+// Arrays - [] , 0 index based
+
+const friends = ["john", "Peter", "Anna", "bob", 45, null];
+console.log(friends);
+
+let bestFriend = friends[3];
+console.log(bestFriend);
+
+friends[3] = "jagmeet";
+console.log(friends[3]);
+console.log(friends);
