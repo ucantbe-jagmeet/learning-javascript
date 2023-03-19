509d69d0bb3101e0a34ce70ff8ef8c547b9ef92d
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-19 16:10:50 +0530

    add

---
+++ john-smilga/14-datatypes/index.js
@@ -0,0 +1,31 @@
+// Data Types - 7 Total
+// primitive - String, Number, Boolean, Null, undefined, Symbol
+// Object - Arrays, Functions, Objects
+
+//typeof - operator ( typeof variable) (typeof value)
+
+// String
+const text = "some text";
+
+// Number
+const number = 45;
+
+// Boolean
+let value1 = true;
+let value2 = false;
+
+// Null
+const result = null;
+
+// Undefined
+let jags;
+
+
+// Symbol(ES6)
+console.log(typeof text);
+console.log(typeof true);
+console.log(typeof number);
+console.log(typeof value1);
+console.log(typeof value2);
+console.log(typeof result);
+console.log(typeof jags);
