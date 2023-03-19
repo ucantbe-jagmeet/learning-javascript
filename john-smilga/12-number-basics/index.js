67d83429cd68e9e5f347d3900625696a6d0de5f8
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-19 16:10:19 +0530

    add

---
+++ john-smilga/12-number-basics/index.js
@@ -0,0 +1,26 @@
+//loosely typed = don't declare type
+
+
+const number = 34;
+const number2 = 2.456; // numbers are blue in color
+const number3 = '2.456'; // string is black in color
+// you will get better idea about type of variables
+
+// console.log(number);
+// console.log(number2);
+// console.log(number3);
+
+const add = number + number2;
+const sub = number - number2;
+const multi = number * number2;
+const divide = number / number2;
+
+
+// +=, -=, *=, ++, --, %
+let num =40;
+num += 5;
+num -= 5;
+num *= 5;
+num ++;
+num --;
+console.log(num);
