a3c39ecd76a9da8b4122f2121d5b3254e0b608b5
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-19 16:08:31 +0530

    add

---
+++ john-smilga/06-variables/index.js
@@ -0,0 +1,21 @@
+// varibale - most basic building block
+// variables - Store , Access, Modify === value
+// Declare , Assignment Operator , Assign value
+
+/* naming conventions  
+    - can contain digits, letters, underscore, $
+    - must start with letter, $ or -/_
+    
+    no keyword - can not start with number
+
+    -- case sensitive - fullName vs Fullname
+    -- camelCase or _underscore
+*/
+
+let name1 = "jagmeet";
+var value = "Some Value";
+const lastName = "Singh"; // const cant be reasign
+
+console.log(value);
+console.log(name1);
+console.log(lastName);
