d5701da4fa85c6bbeba40e517e7dfcaebc5cafa3
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-19 16:08:41 +0530

    add

---
+++ john-smilga/07-assign-variable/app.js
@@ -0,0 +1,12 @@
+// Variable - Most Basic Building Block
+// Variables - Store, Access, Modify === Value
+// Declare, Assignment Operator, Assign Value
+// assign value later, modify existing
+let name = "Jagmeet Singh";
+let address, zip, state;
+address = "101 main street";
+zip = "177201";
+state = "Punjab";
+name = "Jags";
+console.log(address, zip, state);
+console.log(name);
