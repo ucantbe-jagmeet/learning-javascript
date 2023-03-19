a738e5f12dda1b6618094700a0d8b52e333f72e2
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-19 16:10:08 +0530

    add

---
+++ john-smilga/11-string-concat/index.js
@@ -0,0 +1,16 @@
+// String Concatenation - combine string values
+// `` backticks (template strings) easier options
+
+const firstName = 'john';
+const lastName = 'Shakespear';
+
+let fullName = firstName +" " + lastName ;
+
+console.log(firstName +" " + lastName);
+console.log(fullName);
+
+
+const website = 'google';
+const url = "https://" + website + '.com' ;
+
+console.log(url);
