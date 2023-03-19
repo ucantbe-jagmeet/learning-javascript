4dc29434b858af3bd9f45c1880835e07da8b2afc
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-19 16:08:05 +0530

    add

---
+++ john-smilga/05-statement-comments/index.js
@@ -0,0 +1,7 @@
+// statements - set of instructions
+// comments - shortcut command + /
+
+console.log("hello World");
+console.log("hello Sir");
+console.log("hello Mam");
+console.log("Good Evening");
