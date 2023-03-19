511114716e7578eba96aaec83b818f2fd930b0c9
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-19 16:11:24 +0530

    add

---
+++ john-smilga/16-array-challenge/index.js
@@ -0,0 +1,10 @@
+// Arrays
+// Arrays - [] , 0 index based
+
+const fruits = ["apple", "Mango", "Litchi", "Papaya", 45];
+
+const firstFruit = fruits[0];
+fruits[4] = "Orange";
+
+console.log(firstFruit);
+console.log(fruits);
