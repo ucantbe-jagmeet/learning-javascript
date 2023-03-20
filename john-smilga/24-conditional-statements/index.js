cef54ee8032e32bad366dc432fbe2f2ff0c98e6a
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-20 22:04:54 +0530

    add

---
+++ john-smilga/24-conditional-statements/index.js
@@ -0,0 +1,32 @@
+// conditionals Statements
+// >, <, >=, <=, ==, ===, !=, !===
+
+// const value = 2 > 9;
+// const value = 2 > 1;
+// console.log(typeof value);
+
+// if(value)
+// {
+//     console.log('hello World');
+// }
+// else
+// {
+//     console.log('world hello');
+// }
+
+//-------------------------------------------
+
+const num1 = 4;
+const num2 = '4';
+
+if(num1 === num2)
+{
+    console.log('hello World');
+}
+else
+{
+    console.log('OOPS !');
+}
+
+// == equality the value only
+// === equality the value and its type
