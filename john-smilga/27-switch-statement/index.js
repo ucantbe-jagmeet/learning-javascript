be80cb8bbcb7aa900077d41457760f4c5acd8ce2
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-20 22:05:43 +0530

    add

---
+++ john-smilga/27-switch-statement/index.js
@@ -0,0 +1,30 @@
+// switch 
+// dice values 1 - 6
+
+const dice ="";
+
+switch (dice)
+{
+    case 1:
+        console.log('You Got One');
+        break;
+    case 2:
+        console.log('You Got Two');
+        break;
+    case 3:
+        console.log('You Got Three');
+        break;
+    case 4:
+        console.log('You Got Four');
+        break;
+    case 5:
+        console.log('You Got Five');
+        break;
+    case 6:
+        console.log('You Got Six');
+        break;
+
+    default: 
+        console.log('You did not roll the dice');
+
+}
