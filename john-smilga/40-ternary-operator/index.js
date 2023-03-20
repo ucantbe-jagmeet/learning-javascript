6f8918351424ef07f0267d8ac6f4b6fb35442a01
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-20 22:09:30 +0530

    add

---
+++ john-smilga/40-ternary-operator/index.js
@@ -0,0 +1,18 @@
+// unary  binary basically means numebr of operand /s they require to perform action
+
+// unary operator - typeof
+let text = 'some text';
+
+console.log(typeof text);
+
+// binary operator - assignment
+
+let number =3;
+let number2 = 2 + 5;
+
+//ternary operator 
+// condition ? (runs if true ) : (runs if false)
+
+const value = 2 > 3;
+
+value ? console.log(`value is true`):console.log(`value is false`);
