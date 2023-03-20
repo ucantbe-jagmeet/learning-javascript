b58cad998b9f4211f8b1354b08e8d66837df0ce9
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-20 22:08:55 +0530

    add

---
+++ john-smilga/38-null-undef/index.js
@@ -0,0 +1,16 @@
+// Null and undefined
+// Both represet "no value"
+
+// undefined - "javascript can not find the value for this"
+
+// variable without value
+// missing functions arguments
+// missing object properties
+
+// null -> "developer sets the value"
+
+let number = 20 + null; // result will be 20 + 0 ;
+console.log(number);
+
+let number2 = 20 + undefined;
+console.log(number2);
