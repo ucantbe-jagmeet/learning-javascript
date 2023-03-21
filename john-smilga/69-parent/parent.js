504e8d03b03aa6d8a2290fe4d1b5b840ba6d6e47
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-21 21:21:44 +0530

    add

---
+++ john-smilga/69-parent/parent.js
@@ -0,0 +1,5 @@
+// parent element
+// this gives the parent div of selected element
+const heading = document.querySelector("h2");
+const parent = heading.parentElement;
+parent.style.color = "green";
