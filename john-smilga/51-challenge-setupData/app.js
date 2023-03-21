51190283a5b75d66d09118d823edfe24f71d9a46
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-21 21:17:29 +0530

    add

---
+++ john-smilga/51-challenge-setupData/app.js
@@ -0,0 +1,34 @@
+let students = [
+  {
+    id: 1,
+    name: "peter",
+    score: "80",
+    favouriteSubject: "math",
+  },
+  {
+    id: 2,
+    name: "suzan",
+    score: "90",
+    favouriteSubject: "history",
+  },
+  {
+    id: 3,
+    name: "bob",
+    score: "75",
+    favouriteSubject: "math",
+  },
+  {
+    id: 4,
+    name: "anna",
+    score: "99",
+    favouriteSubject: "art",
+  },
+  {
+    id: 5,
+    name: "peter",
+    score: "92",
+    favouriteSubject: "math",
+  },
+];
+
+console.log(students);
