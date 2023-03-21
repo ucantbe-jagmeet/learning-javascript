714fa067d4dab47599e2a032bd711ad2c379024c
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-21 21:19:38 +0530

    add

---
+++ john-smilga/59-math-object/index.js
@@ -0,0 +1,20 @@
+// math
+// Standard built-in Objects - always available
+
+const num = 4.56789;
+// const num = 8.999999;
+
+// const num1= 25;
+const num1 = 29;
+
+// const result = Math.floor(num);
+// const result = Math.ceil(num);
+
+// const result = Math.sqrt(num1);
+// const result = Math.PI
+// const result = Math.min(4,2,5,6,8,3,1);
+// const result = Math.max(4,2,5,6,8,3,1);
+
+const result = Math.floor(Math.random() * 100);
+
+console.log(result);
