d8d1fc53c087a3ac75b0fd8450a604b532142595
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-22 21:54:43 +0530

    add

---
+++ john-smilga/79-change-css/index.js
@@ -0,0 +1,10 @@
+//  styling Css 
+
+
+const random = document.querySelector('.random')
+// random.style.backgroundColor = 'blue';
+// random.style.color = 'white';
+// random.style.fontSize = '3rem';
+// random.style.textTransform = 'Capitalize';
+
+random.classList.add('title')
