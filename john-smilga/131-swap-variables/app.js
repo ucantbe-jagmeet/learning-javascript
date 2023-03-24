4c49660e1ba123d04118788aee63efbabe71a720
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-24 21:54:46 +0530

    add

---
+++ john-smilga/131-swap-variables/app.js
@@ -0,0 +1,15 @@
+// Destructuring
+// faster / easier way to access/unpack values from arrays
+// objects into variables
+// Arrays
+
+let first = "bob";
+let second = "john";
+
+// let temp = second;
+// second = first;
+// first = temp;
+
+[second, first] = [first, second];
+
+console.log(first, second);
