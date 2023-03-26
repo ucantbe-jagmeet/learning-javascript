84cebea3261e11ef1c612d0aecde01cfb75f0f53
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-26 20:32:03 +0530

    add

---
+++ john-smilga/152-named-export/app.js
@@ -0,0 +1,19 @@
+import { random, people } from "./utils/data";
+console.log(random);
+
+const container = document.querySelector(".container");
+const btn = document.querySelector(".btn");
+
+const showPeople = () => {
+  const newPeople = people
+    .map((person) => {
+      const { name, job } = person;
+      return ` <p> ${name} <strong>${job}</strong></p>`;
+    })
+    .join("");
+  container.innerHTML = newPeople;
+};
+
+btn.addEventListener("click", () => {
+  showPeople();
+});
