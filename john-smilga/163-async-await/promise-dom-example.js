2adce61f85945bcdc521a48ea5f51305d633383a
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-26 20:34:44 +0530

    add

---
+++ john-smilga/163-async-await/promise-dom-example.js
@@ -0,0 +1,28 @@
+// callbacks, promises, async/ await
+// promises - Pending, Resolved, Rejected
+// then catch - pass another callback
+
+const heading1 = document.querySelector(".one");
+const heading2 = document.querySelector(".two");
+const heading3 = document.querySelector(".three");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+  addColor(1000, heading1, "red")
+    .then(() => addColor(2000, heading2, "green"))
+    .then(() => addColor(2000, heading3, "blue"))
+    .catch((err) => console.log(err));
+});
+
+function addColor(time, element, color) {
+  return new Promise((resolve, reject) => {
+    if (element) {
+      setTimeout(() => {
+        element.style.color = color;
+        resolve();
+      }, time);
+    } else {
+      reject(new Error(`there is not such ${element}`));
+    }
+  });
+}
