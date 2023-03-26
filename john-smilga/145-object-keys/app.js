3366ca98cd85101a3f8b85d148262ff727db5c5d
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-26 20:30:44 +0530

    add

---
+++ john-smilga/145-object-keys/app.js
@@ -0,0 +1,15 @@
+/*
+ three methods to convert objects into arrays
+ -> object.keys() -> convert property names into array
+ -> object.values() -> convert property vales into array
+ -> object.entries() -> convert both
+*/
+
+const person = {
+  name: "john",
+  age: 35,
+  staus: "student",
+};
+
+const keys = Object.keys(person);
+console.log(keys);
